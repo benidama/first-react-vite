773e055c274e3cce0d46c8a5a0e8e66b53960501
benidama <benidama02@gmail.com>
2024-10-29 21:23:20 +0000

    My first react vite with tailwindcss

---
+++ src/Vite.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Vite = () => {
+  return (
+    <>
+      <div className="flex flex-col justify-center items-center gap-3">
+        <h2 className="text-cyan-500 text-3xl">
+          This is my first react vite with tailwind
+        </h2>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Optio,
+          itaque. Aut autem magnam molestias quis!
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default Vite;
