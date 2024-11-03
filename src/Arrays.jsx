309111e055d237ce98cb909409b079a3329a93a8
benidama <benidama02@gmail.com>
2024-11-03 20:33:22 +0000

    Add arry file

---
+++ src/Arrays.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Arrays = () => {
+  return (
+    <div>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus
+      fugiat iure libero.
+      <h1></h1>
+    </div>
+  );
+};
+
+export default Arrays;
