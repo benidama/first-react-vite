b4e68e0b84faed71179585e8fd4cf8efa29118cc
benidama <benidama02@gmail.com>
2024-11-06 22:02:10 +0000

    Addition

--- src/Arrays.jsx
+++ src/Arrays.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-
 const Arrays = () => {
   const arrayReact = ["Martin", "John", "Peter", "Quintus"];
   const remove = arrayReact.splice(2, 1, "Emmy", "Kanuma");
@@ -7,9 +6,9 @@ const Arrays = () => {
     ["Paul", "Ntaki", "Gad"],
     ["Yuhi", "Claude"],
   ];
-
+  const map = arrayReact.map((f) => <p>{f}</p>);
   return (
-    <div>
+    <div className="flex flex-col justify-center items-center">
       <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h1>
       <p className="text-orange-400">
         Hello world, my name is {arrayReact[3]} and I have {arrayReact.length}{" "}
@@ -21,7 +20,7 @@ const Arrays = () => {
       </h3>
       <p>Lorem ipsum dolor sit amet {remove}.</p>
       <h1>How do you do my friends?</h1>
-      <p>
+      <p className="text-xs w-[630px] text-center">
         Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dicta impedit
         cupiditate reiciendis repellendus dolores quibusdam recusandae. Officiis
         quisquam laboriosam consequatur voluptas, placeat architecto in, quos
@@ -29,6 +28,11 @@ const Arrays = () => {
       </p>
       <br />
       <p>
+        This is the list of my friends:
+        <span className="text-blue-500">{map}</span>
+      </p>
+      <br />
+      <p className="text-xs w-[650px]">
         Lorem ipsum dolor, sit amet consectetur adipisicing elit. Mollitia
         obcaecati laudantium aperiam. Eos expedita, dolore totam neque molestias
         et itaque? Iure sed laboriosam reiciendis necessitatibus eum architecto
