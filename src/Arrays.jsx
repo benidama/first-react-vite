04204fc651e72c75eac1a4734d0bf0362cf4ed12
benidama <benidama02@gmail.com>
2024-11-09 18:06:09 +0000

    Deep change on my project

--- src/Arrays.jsx
+++ src/Arrays.jsx
@@ -2,24 +2,32 @@ import React from "react";
 const Arrays = () => {
   const arrayReact = ["Martin", "John", "Peter", "Quintus"];
   const remove = arrayReact.splice(2, 1, "Emmy", "Kanuma");
+  const num = [24, 56, 66, 134];
+  const reducer = num.reduce((sum, el) => sum + el);
   const flating = [
     ["Paul", "Ntaki", "Gad"],
     ["Yuhi", "Claude"],
   ];
-  const foreac = arrayReact.forEach((el, yu, ko) => el + "ok");
+  const foreac = arrayReact.map((el, yu, ko) => el + " " + "ok").join(", ");
   const map = arrayReact.map((f) => <p>{f}</p>);
+  const flter = arrayReact.filter((el) => el.length > 6).join();
   return (
-    <div className="flex flex-col justify-center items-center">
-      <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h1>
+    <div className="flex flex-col justify-center items-center pt-5">
+      <h1 className="text-2xl text-gray-600 mb-3">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+      </h1>
       <p className="text-orange-400">
         Hello world, my name is {arrayReact[3]} and I have {arrayReact.length}{" "}
         children
       </p>
-      <p>{foreac}</p>
+      <p className="text-violet-700 m-3">{foreac}</p>
       <h3>
         My name is {flating[1][1]} and my brothers are{" "}
         {flating.flat().join(",")}.
       </h3>
+      <p className="text-violet-800 p-3">
+        {flter} has {reducer} friends
+      </p>
       <p>Lorem ipsum dolor sit amet {remove}.</p>
       <h1>How do you do my friends?</h1>
       <p className="text-xs w-[630px] text-center">
