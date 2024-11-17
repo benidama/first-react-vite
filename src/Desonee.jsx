77df7d8790f8f265d335adb634f7bd1cd7d350e4
benidama <benidama02@gmail.com>
2024-11-17 22:14:24 +0000

    CHANGED

--- src/Desonee.jsx
+++ src/Desonee.jsx
@@ -1,15 +1,26 @@
 import React from "react";
 
+function List() {
+  return <li>I have {prp.age} ages</li>;
+}
+
 const Desonee = () => {
-  //   const destru = [23, 3, 56, 13];
-  //   const [a, , b] = destru;
+  const destru = [23, 3, 56, 13];
+
   return (
     <div>
       <h1>I have 67yrs old and 6 children.</h1>
+
       <p>
         This is why we are here to tell you all about us & to know how you like
         our products on the market especially this modern market.
       </p>
+      <button onClick={() => alert("Welcome")}>Click me</button>
+      <ul>
+        {destru.map((ag) => (
+          <List age={ag} />
+        ))}
+      </ul>
     </div>
   );
 };
