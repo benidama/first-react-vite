98d27d6cc225030125d7cc9503195730f3414543
benidama <benidama02@gmail.com>
2024-11-17 19:30:24 +0000

    Add file

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,16 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import Vite from "./vite";
 import Arrays from "./Arrays";
+import Desonee from "./Desonee";
 
 function App() {
-  const [count, setCount] = useState(0);
+  <>
+    <h1 className="text-2xl text-gray-500">Vite + React</h1>
 
-  return (
-    <>
-      <div className="flex flex-row justify-center items-center">
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1 className="text-2xl text-gray-500">Vite + React</h1>
-      <div className="card">
-        <button
-          className="text-center text-red-500"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          count is <span className="text-cyan-600 text-3xl"> {count}</span>
-        </button>
-      </div>
-      <Vite />
-      <Arrays />
-    </>
-  );
+    <Vite />
+    <Arrays />
+    <Desonee />
+  </>;
 }
 
 export default App;
---
+++ src/Desonee.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Desonee = () => {
+  //   const destru = [23, 3, 56, 13];
+  //   const [a, , b] = destru;
+  return (
+    <div>
+      <h1>I have 67yrs old and 6 children.</h1>
+      <p>
+        This is why we are here to tell you all about us & to know how you like
+        our products on the market especially this modern market.
+      </p>
+    </div>
+  );
+};
+
+export default Desonee;
